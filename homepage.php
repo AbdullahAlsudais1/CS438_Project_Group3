4f677c99ed39ea6a81de8debacc47d1413c158a1
AbdullahAlsudais1 <asalssudais@sm.imamu.edu.sa>
2021-03-23 16:57:47 +0300

    change the position of logout button

--- homepage.php
+++ homepage.php
@@ -1,19 +1,22 @@
 <?php  include 'database and php files/CheckDB.php';?>
 <!DOCTYPE html>
-<html lang="en">
+
   <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
     <title>Quiz</title>
-    <link rel="stylesheet" href="homepage.css">
-    <link rel="stylesheet" href="quizpage.css">
+    <link rel="stylesheet" type="text/css" href="homepage.css?v=<?php echo time(); ?>">
   </head>
   <body>
-    
-    <h3> <?php echo "Welcome &nbsp;".  $_SESSION['FName'],"&nbsp;" . $_SESSION['LName'] ." &nbsp;to quiz app";?></h3>
-  <center>  <a  class="button" href="Register and SignIN/logout.php">Logout</a></center>
-    <hr>
+
+   <div class="button1"><a  class="button" href="Register and SignIN/logout.php">Logout</a></div>
+   <h3>Home page</h3>
+   <hr>
+  <div class="Profile">
+    <h3>Profile</h3>
+        <p><strong>Name: </strong> <?php echo  $_SESSION['FName'],"&nbsp;" . $_SESSION['LName']; ?></p> <br><br>
+        <p><strong>Email:</strong>  <?php echo $_SESSION['Email'];   ?></p>
+  </div>
+
     <div class="container">
       <div class="flex-center flex-column">
         <h1>Quiz</h1>
